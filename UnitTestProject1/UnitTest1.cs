0d15b9ef4a1c644e9ef897e3d97533766f805aba
sinhtm <sinhtm@DESKTOP-VSLKJFA>
2016-10-14 15:46:24 +0700

    Add project files.

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+//using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using NUnit.Framework;
+
+namespace UnitTestProject1
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            ClassLibrary1.Class1 obj = new ClassLibrary1.Class1();
+            int res= obj.add(5,4);
+            Assert.That(res,Is.EqualTo(9));
+        }
+    }
+}
